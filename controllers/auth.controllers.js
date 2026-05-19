c92a4da0da3fe1b216ee2a24bec896dbcd2a6684
John <johnwick@gmail.com>
2026-05-20 00:27:42 +0530

    Add signOut

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -120,5 +120,15 @@ export const signIn = async (req, res, next) => {
 
 
 export const signOut = async (req, res, next) => {
-    // Implement sign out logic here
+    
+    try {
+        res.status(200).json({
+            success: true,
+            message: "User signed out successfully"
+        });
+
+    }catch(error) {
+        next(error);
+    }
+    
 }
